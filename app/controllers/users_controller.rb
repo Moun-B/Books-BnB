a3d76cecf4b2ec99ad60cdc01823ee5207f1589a
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-08-15 23:14:45 +0900

    finished creation users-controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @rentals = @user.rentals
+    @offers = @user.offers
+  end
+end
