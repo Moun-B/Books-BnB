2385d77cda41af16b08f3da7b9af522013633950
Mounir EL JAMGHILI <eljmounir@gmail.com>
2022-08-15 23:26:31 +0900

    finished correcting route users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  devise_for :users
 
-  devise_for :users [:show] do
+  resources :users, only: [:show] do
     resources :rentals, only: [:new, :create, :edit, :update]
   end
 
