c4e34899a0e80c8f7c7c61f40eda95963f5d0329
Soufiane Ezzine <ezzine.soufiane@gmail.com>
2022-08-17 00:56:33 +0900

    added a book search form to retrieve information from google book API # now view

---
+++ app/policies/book_policy.rb
@@ -0,0 +1,12 @@
+class BookPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+
+  def search?
+    true
+  end
+end
