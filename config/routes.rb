9946e42380dd22e37ffd621551cc3f6306f9a14a
Soufiane Ezzine <ezzine.soufiane@gmail.com>
2022-08-16 12:18:48 +0900

    routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   devise_for :users
 
-  get '/dashboard', to: 'users#dashbaord', as: :dashboard
+  get '/dashboard', to: 'users#dashboard', as: :dashboard
 
   resources :offers, only: [:index, :show, :new, :create] do
     resources :rentals, only: [:create]
