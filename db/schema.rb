226d47849f5397b0080f7ed8e4b9f40dfac1d44f
Soufiane Ezzine <ezzine.soufiane@gmail.com>
2022-08-17 13:38:06 +0900

    fixed the seed and the show page with right pictures

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_15_071615) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_17_031155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_15_071615) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.string "cover_url"
   end
 
   create_table "offers", force: :cascade do |t|
