4ea651a5fac82796cd20f34148771ca72d1c0798
Kyle Jeffrey <kyle.m.jeffrey@gmail.com>
2022-08-18 14:03:27 +0900

    implemented dynamic rental days and total price in offers#show page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,7 +4,7 @@ import "./controllers"
 import "bootstrap"
 
 import { Application } from "@hotwired/stimulus"
-// import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
 window.Stimulus = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
--- app/javascript/controllers/total_price_controller.js
+++ app/javascript/controllers/total_price_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="total-price"
 export default class extends Controller {
-  static targets = ["days", "start", "end"]
+  static targets = ["days", "start", "end", "price", "total", "priceInTotal"]
 
   connect() {
     // console.log("Hello from total_price_controller!", this.element)
@@ -11,38 +11,17 @@ export default class extends Controller {
   calculateDays() {
     // console.log((this.startTarget.value))
 
-    let start = this.startTarget.value;
-    let end = this.endTarget.value;
+    let start = Date.parse(this.startTarget.value);
+    let end = Date.parse(this.endTarget.value);
+    const price = this.priceTarget.innerText;
 
-    start = this.formatDate(start);
-    end = this.formatDate(end);
-
-
-    start = new Date(start);
-    end = new Date(end);
-
-    daysDiff = this.getDifferenceInDays(end - start);
-
-    // console.log(start)
-    // console.log(end)
-    console.log(daysDiff)
-
-    // console.log(this.endTarget.value)
-    // this.daysTarget.innerText = this.endTarget.value - this.startTarget.value;
-  }
-
-  formatDate(date) {
-    var d = new Date(date),
-      month = '' + (d.getMonth() + 1),
-      day = '' + d.getDate(),
-      year = d.getFullYear();
-
-    if (month.length < 2)
-      month = '0' + month;
-    if (day.length < 2)
-      day = '0' + day;
-
-    return [year, month, day].join('-');
+    if (start && end) {
+      const daysDiff = this.getDifferenceInDays(end, start);
+      this.daysTarget.innerText = daysDiff;
+      const total = price * daysDiff;
+      this.priceInTotalTarget.innerHTML = `¥${price}`;
+      this.totalTarget.innerHTML = `¥${total}`;
+    }
   }
 
   getDifferenceInDays(date1, date2) {
